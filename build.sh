1efbddff9bc7fde72636c24434ed8e3ced74620b
4li kh4n <47796965+D3vilhun73r404@users.noreply.github.com>
2019-11-28 13:02:48 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 import os
 import time
-kali = input("YOU WANT TO UPDATE SYSTEM : Y / n ")
+print('''
+
+
+$$\   $$\  $$$$$$\  $$\       $$$$$$\       $$$$$$$$\  $$$$$$\   $$$$$$\  $$\       
+$$ | $$  |$$  __$$\ $$ |      \_$$  _|      \__$$  __|$$  __$$\ $$  __$$\ $$ |      
+$$ |$$  / $$ /  $$ |$$ |        $$ |           $$ |   $$ /  $$ |$$ /  $$ |$$ |      
+$$$$$  /  $$$$$$$$ |$$ |        $$ |           $$ |   $$ |  $$ |$$ |  $$ |$$ |      
+$$  $$<   $$  __$$ |$$ |        $$ |           $$ |   $$ |  $$ |$$ |  $$ |$$ |      
+$$ |\$$\  $$ |  $$ |$$ |        $$ |           $$ |   $$ |  $$ |$$ |  $$ |$$ |      
+$$ | \$$\ $$ |  $$ |$$$$$$$$\ $$$$$$\          $$ |    $$$$$$  | $$$$$$  |$$$$$$$$\ 
+\__|  \__|\__|  \__|\________|\______|         \__|    \______/  \______/ \________|
+                                                                                    
+            CODED BY ALI KHAN BANGLADESHI HACKER (RANDOMALIEXPLOITER)                                                                        
+                                                                                    
+''')
+
+kali = input("YOU WANT TO UPDATE SYSTEM : YES / NO ")
 
 if "Y" in kali:
 	os.system("locale-gen en_US.UTF-8")
@@ -9,7 +25,56 @@ if "Y" in kali:
 	os.system("cd /etc/apt/sources.list.d && rm -rf * && cd .. && rm -rf sources.list")
 	time.sleep(5)
 	print("KALI LINUX REPO ADDING.......")
-	os.system("echo '# Kali linux repositories | Added by Katoolin\ndeb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list")
+	os.system("echo '# Kali linux repositories | Added by Ali Khan\ndeb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list")
+	os.system("echo '# Kali linux repositories | Added by Ali Khan\ndeb-src http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list")
+	time.sleep(5)
+	print("KALI LINUX PGP ADDING........")
+	os.system("wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-key.asc")
+	print("KALI LINUX UPDATEING......")
+	os.system("sudo apt update --fix-missing")
+	print("INSTALLING TOOL ........")
+	os.system("apt install wpscan sqlmap python python-pip python3 python3-pip php php-curl curl nodejs -y")
+elif "YES" in kali:
+	os.system("locale-gen en_US.UTF-8")
+	os.system("apt install dialog -y")
+	os.system("apt install nano -y")
+	os.system("cd /etc/apt/sources.list.d && rm -rf * && cd .. && rm -rf sources.list")
+	time.sleep(5)
+	print("KALI LINUX REPO ADDING.......")
+	os.system("echo '# Kali linux repositories | Added by Ali Khan\ndeb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list")
+	os.system("echo '# Kali linux repositories | Added by Ali Khan\ndeb-src http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list")
+	time.sleep(5)
+	print("KALI LINUX PGP ADDING........")
+	os.system("wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-key.asc")
+	print("KALI LINUX UPDATEING......")
+	os.system("sudo apt update --fix-missing")
+	print("INSTALLING TOOL ........")
+	os.system("apt install wpscan sqlmap python python-pip python3 python3-pip php php-curl curl nodejs -y")
+elif "y" in kali:
+	os.system("locale-gen en_US.UTF-8")
+	os.system("apt install dialog -y")
+	os.system("apt install nano -y")
+	os.system("cd /etc/apt/sources.list.d && rm -rf * && cd .. && rm -rf sources.list")
+	time.sleep(5)
+	print("KALI LINUX REPO ADDING.......")
+	os.system("echo '# Kali linux repositories | Added by Ali Khan\ndeb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list")
+	os.system("echo '# Kali linux repositories | Added by Ali Khan\ndeb-src http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list")
+	time.sleep(5)
+	print("KALI LINUX PGP ADDING........")
+	os.system("wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-key.asc")
+	print("KALI LINUX UPDATEING......")
+	os.system("sudo apt update --fix-missing")
+	print("INSTALLING TOOL ........")
+	os.system("apt install wpscan sqlmap python python-pip python3 python3-pip php php-curl curl nodejs -y")
+elif "yes" in kali:
+	os.system("locale-gen en_US.UTF-8")
+	os.system("apt install dialog -y")
+	os.system("apt install nano -y")
+	os.system("cd /etc/apt/sources.list.d && rm -rf * && cd .. && rm -rf sources.list")
+	time.sleep(5)
+	print("KALI LINUX REPO ADDING.......")
+	os.system("echo '# Kali linux repositories | Added by Ali Khan\ndeb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list")
+	os.system("echo '# Kali linux repositories | Added by Ali Khan\ndeb-src http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list")
 	time.sleep(5)
 	print("KALI LINUX PGP ADDING........")
 	os.system("wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-key.asc")
