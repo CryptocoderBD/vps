cdd2c027c086e9fa05b4b2c3ad4496eef47dc89f
4li kh4n <47796965+D3vilhun73r404@users.noreply.github.com>
2019-11-28 11:40:46 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ cd /etc/apt/sources.list.d
 rm -rf *
 cd ..
 rm -rf sources.list
+echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib\ndeb-src http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list
 wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-key.asc
-sudo nano sources.list
-deb http://http.kali.org/kali kali-rolling main non-free contrib
-deb-src http://http.kali.org/kali kali-rolling main non-free contrib
 sudo apt update
 sudo apt install wpscan sqlmap python2 python2-pip python3 python3-pip php php-curl curl nodejs --yes
