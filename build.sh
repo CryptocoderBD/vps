4c73931cb05bb510a10d6d6d8d64f715afe23a86
4li kh4n <47796965+D3vilhun73r404@users.noreply.github.com>
2019-11-28 11:53:42 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 sudo locale-gen en_US en_US.UTF-8
 sudo apt install dialog -y
 sudo apt install nano -y
-cd /etc/apt/sources.list.d
-rm -rf *
-cd ..
-rm -rf sources.list
-echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list
-echo 'deb-src http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list
-wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-key.asc
+sudo cd /etc/apt/sources.list.d
+sudo rm -rf *
+sudo cd ..
+sudo rm -rf sources.list
+sudo echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list
+sudo echo 'deb-src http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list
+sudo wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-key.asc
 sudo apt update
 sudo apt install wpscan sqlmap python2 python2-pip python3 python3-pip php php-curl curl nodejs -y
