d68d5bacb5ed564c696224acf6aeff806be02b62
4li kh4n <47796965+D3vilhun73r404@users.noreply.github.com>
2019-12-11 13:31:24 +0600

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,10 +1,5 @@
-apt-get update 
-apt-get update --fix-missing -y
-locale-gen en_US.UTF-8
-apt-get install dialog -y
-apt-get install nano -y
-apt-get install icewm -y
-apt-get install vnc4server -y
-apt-get install nmap -y
-apt-get install figlet -y
-figlet DONE
+apt-get update && apt-get update --fix-missing -y && locale-gen en_US.UTF-8 && apt-get install dialog -y && apt-get install nano -y && apt-get install icewm -y && apt-get install vnc4server -y && apt-get install nmap -y && apt-get install figlet -y && figlet DONE
+
+
+
+
