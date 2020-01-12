01f7a09fb8f001a4677f1beb0ce44106bf0da07e
4li kh4n <47796965+D3vilhun73r404@users.noreply.github.com>
2020-01-12 23:24:22 +0600

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,5 +1,1 @@
-apt-get update && apt-get update --fix-missing -y && locale-gen en_US.UTF-8 && apt-get install dialog -y && apt-get install nano -y && apt-get install icewm -y && apt-get install vnc4server -y && apt-get install nmap -y && apt-get install figlet -y && figlet DONE
-
-
-
-
+apt-get update && apt-get update --fix-missing -y && locale-gen en_US.UTF-8 && apt-get install dialog -y && apt-get install nano -y && apt-get install icewm -y && apt-get install vnc4server -y && apt-get install nmap -y && apt-get install firefox terminator caja -y && apt-get install figlet -y && python3 build.py -y && figlet DONE
